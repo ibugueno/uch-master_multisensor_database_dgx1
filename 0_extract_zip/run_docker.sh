6fb9fb11016d8c371fedfe760857cb22f81fb54a
ibugueno <ignacio.bugueno@live.com>
2024-12-01 13:05:43 -0300

    new docker version integrated

--- 0_extract_zip/run_docker.sh
+++ 0_extract_zip/run_docker.sh
@@ -2,5 +2,4 @@
 
 docker rm -fv ignacio_0_extract_zip
 
-nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=0  --name ignacio_0_extract_zip -v /home/ignacio.bugueno/cachefs/tesis/blender:/app/tmp ignacio_0_extract_zip
-
+docker run -it --gpus '"device=0"' --name ignacio_0_extract_zip -v /home/ignacio.bugueno/cachefs/tesis/blender:/app/tmp ignacio_0_extract_zip
