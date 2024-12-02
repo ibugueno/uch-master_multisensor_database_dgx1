05f0f8c4e5b9a512503afe25b5050805c60d8b97
ibugueno <ignacio.bugueno@live.com>
2024-12-02 00:54:39 -0300

    run_v2e.sh added

--- 1_v2e/run_docker.sh
+++ 1_v2e/run_docker.sh
@@ -2,4 +2,4 @@
 
 docker rm -fv ignacio_1_v2e
 
-docker run -it --gpus '"device=0"' --name ignacio_1_v2e -v /home/ignacio.bugueno/cachefs/tesis/blender:/app/input ignacio_1_v2e
+docker run -it --gpus '"device=0"' --name ignacio_1_v2e -v /home/ignacio.bugueno/cachefs/tesis/blender:/app/input -v /home/ignacio.bugueno/cachefs/tesis/data/blender_raw_events:/app/output ignacio_1_v2e
---
+++ 1_v2e/run_v2e.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Archivo que contiene los directorios
+INPUT_FILE="input/events_folders.txt"
+
+# Activar el entorno conda
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate v2e
+
+# Leer cada línea del archivo
+while IFS= read -r line; do
+    # Determinar height y width según el nombre del directorio
+    if [[ "$line" == *"davis346"* ]]; then
+        height=260
+        width=346
+    elif [[ "$line" == *"evk4"* ]]; then
+        height=720
+        width=1280
+    else
+        echo "No se reconoció el tipo de directorio para: $line"
+        continue
+    fi
+
+    # Iterar sobre los modelos: clean y noisy
+    #for model in clean noisy; do
+    for model in clean; do
+        # Construir los directorios de salida
+        output_folder="output/$model/$line"
+
+        # Ejecutar el comando
+        python v2e.py -i "input/$line" \
+            --overwrite \
+            --input_frame_rate=1000 \
+            --auto_timestamp_resolution=True \
+            --dvs_exposure duration 0.005 \
+            --output_folder="$output_folder" \
+            --pos_thres=.15 \
+            --neg_thres=.15 \
+            --sigma_thres=0 \
+            --output_width=$width \
+            --output_height=$height \
+            --disable_slomo \
+            --no_preview \
+            --skip_video_output \
+            --dvs_aedat4 events.aedat4 \
+            --dvs_params "$model"
+    done
+
+done < "$INPUT_FILE"
