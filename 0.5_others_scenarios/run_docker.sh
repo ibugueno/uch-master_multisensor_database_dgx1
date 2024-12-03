a2069967db47189884425df6a304a5f54fd5ca85
ibugueno <ignacio.bugueno@live.com>
2024-12-03 02:51:25 -0300

    Dockerfile path fixed

--- 0.5_others_scenarios/run_docker.sh
+++ 0.5_others_scenarios/run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker rm -fv ignacio_0.5_others_scenarios
+docker rm -fv ignacio_0.5_others_scenarios_2
 
-docker run -it --gpus '"device=1"' --name ignacio_0.5_others_scenarios -v /home/ignacio.bugueno/cachefs/tesis/blender:/app/input -v /home/ignacio.bugueno/cachefs/tesis/data/ddbb-s/2_without_back_blur:/app/output ignacio_0.5_others_scenarios
+docker run -it --gpus '"device=1"' --name ignacio_0.5_others_scenarios_2 -v /home/ignacio.bugueno/cachefs/tesis/blender:/app/input -v /home/ignacio.bugueno/cachefs/tesis/data/ddbb-s:/app/output ignacio_0.5_others_scenarios_2
